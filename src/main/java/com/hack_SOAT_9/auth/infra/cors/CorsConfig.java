7823bc9b139d0871ee38ded9281d6b0c587d15eb
Jonathan Ferreira <jhouus33@gmail.com>
2025-07-13 00:09:49 -0300

    ajuste cors

--- src/main/java/com/hack_SOAT_9/auth/infra/cors/CorsConfig.java
+++ src/main/java/com/hack_SOAT_9/auth/infra/cors/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "http://localhost:8080"
+                )
                 .allowedMethods("GET", "POST");
     }
 }
