4cbf287ccf832e4fecbd1a812b2d2a452532f703
Jonathan Ferreira <jhouus33@gmail.com>
2025-07-15 00:37:52 -0300

    fix: ajsute dockercompose

--- src/main/java/com/hack_SOAT_9/auth/infra/cors/CorsConfig.java
+++ src/main/java/com/hack_SOAT_9/auth/infra/cors/CorsConfig.java
@@ -14,6 +14,6 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://localhost:4200",
                         "http://localhost:8080"
                 )
-                .allowedMethods("GET", "POST");
+                .allowedMethods("GET", "POST", "OPTIONS");
     }
 }
